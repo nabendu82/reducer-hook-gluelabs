bd03bfee30563cc853083dc36a0221f7eb230b34
nabendu82 <nabendu.biswas@gmail.com>
2022-04-22 12:02:27 +0530

    Final commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import CounterTwo from './components/CounterTwo';
+import DataFetching from './components/DataFetching';
+
+function App() {
+  return (
+    <div className="App">
+      <CounterTwo />
+      <DataFetching />
+    </div>
+  );
+}
+
+export default App;
